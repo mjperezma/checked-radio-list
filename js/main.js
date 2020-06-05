51a8bea027ae41ca168b8eae0b03d0e73b61d99f
mj0se <mariajose1907@gmail.com>
2020-06-05 19:16:47 +0200

    add code to do list with radio checked

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(ev) {
+  console.log(ev);
+  //  check if they had the shift key down
+  //  and check that they are checking it
+  let inBetween = false;
+  if (ev.shiftKey && this.checked) {
+    // go ahead and do what we please
+    // loop over every single checkbox
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+}
+checkboxes.forEach((checkbox) => checkbox.addEventListener('click', handleCheck));
